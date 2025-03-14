f281455aaaef12641a28fe68e3c139b43bbf8bd2
Andrew Batallones <batallonesa@gmail.com>
2025-03-14 10:55:49 -0400

    Move handlers to separate package

---
+++ handlers/index.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "{\"msg\": \"hi\"}")
+}
--- main.go
+++ main.go
@@ -5,30 +5,16 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/andrewbatallones/api/handlers"
 	"github.com/andrewbatallones/api/utils"
 )
 
 func main() {
 	port := utils.GetEnv("port", "8080")
-	http.HandleFunc("/", index)
-	http.HandleFunc("/healthcheck", healthcheck)
+	http.HandleFunc("/", handlers.Index)
+	http.HandleFunc("/healthcheck", handlers.Healthcheck)
 
 	fmt.Printf("Starting server at port %s", port)
 	fmt.Println()
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, "{\"msg\": \"hi\"}")
-}
-
-func healthcheck(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, "{\"status\": \"ok\"}")
 }
