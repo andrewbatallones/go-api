fbc3b265a2a6d732365eac960d4182d5c5bc135f
Andrew Batallones <batallonesa@gmail.com>
2025-03-14 10:37:32 -0400

    Restructure utils w/ healthcheck handler

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/andrewbatallones/api/utils"
 )
 
 func main() {
-	port := GetEnv("port", "8080")
+	port := utils.GetEnv("port", "8080")
 	fmt.Printf("Starting server at port %s", port)
 	http.HandleFunc("/", index)
+	http.HandleFunc("/healthecheck", healthcheck)
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
@@ -17,4 +20,9 @@ func main() {
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, "{\"msg\": \"hi\"}")
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "{\"status\": \"ok\"}")
 }
