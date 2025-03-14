955e3e7e20d03e3cbb1989eca8dd027e62bc1109
Andrew Batallones <batallonesa@gmail.com>
2025-03-14 12:04:53 -0400

    Introduced pgx w/ updated healthcheck

--- handlers/healthcheck.go
+++ handlers/healthcheck.go
@@ -1,11 +1,39 @@
 package handlers
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func Healthcheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, "{\"status\": \"ok\"}")
+	status := "ok"
+
+	if !testConnection() {
+		status = "database is unable to connect"
+	}
+
+	fmt.Fprintf(w, "{\"status\": \"%s\"}", status)
+}
+
+func testConnection() bool {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		return false
+	}
+	defer conn.Close(context.Background())
+
+	var test string
+	err = conn.QueryRow(context.Background(), "SELECT 'Testing'").Scan(&test)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		return false
+	}
+
+	return true
 }
